cc5f93fb61e14b94c5938f16569952ee94a6ad58
AntonPoludnitsin <33234187+AntonPoludnitsin@users.noreply.github.com>
2020-05-12 16:02:04 +0400

    fix import

--- src/components/ControlBlock.jsx
+++ src/components/ControlBlock.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import Button from "./Button"
 import "../App.css";
 
-class ControlBlock extends React.PureComponent {
+class ControlBlock extends PureComponent {
   render = ({ inc, reset, dec } = this.props) => {
        
 
